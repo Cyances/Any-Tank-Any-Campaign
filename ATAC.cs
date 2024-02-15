0422a03de4c6784a160baca285dbb4a2f20f4178
Cyances <qpsibojos@gmail.com>
2024-02-15 18:48:08 +0800

    Update ATAC.cs

--- ATAC.cs
+++ ATAC.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 using System.Reflection;
 using BehaviorDesigner.Runtime.Tasks.Unity.UnityString;
 
-[assembly: MelonInfo(typeof(ATACMod), "Any Tank Any Campaign Beta", "0.1.1", "Cyance")]
+[assembly: MelonInfo(typeof(ATACMod), "Any Tank Any Campaign Beta", "0.2.0", "Cyance and Fractal")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace ATAC
@@ -43,6 +43,8 @@ namespace ATAC
         public static MelonPreferences_Entry<int> bmp1_chance;
         public static MelonPreferences_Entry<string> bmp1p_replace;
         public static MelonPreferences_Entry<int> bmp1p_chance;
+        public static MelonPreferences_Entry<string> bmp2_replace;
+        public static MelonPreferences_Entry<int> bmp2_chance;
         public static MelonPreferences_Entry<string> t55a_replace;
         public static MelonPreferences_Entry<int> t55a_chance;
         public static MelonPreferences_Entry<string> t72m_replace;
@@ -62,6 +64,8 @@ namespace ATAC
         public static MelonPreferences_Entry<int> bmp1_sa_chance;
         public static MelonPreferences_Entry<string> bmp1p_sa_replace;
         public static MelonPreferences_Entry<int> bmp1p_sa_chance;
+        public static MelonPreferences_Entry<string> bmp2_sa_replace;
+        public static MelonPreferences_Entry<int> bmp2_sa_chance;
         public static MelonPreferences_Entry<string> t64a_replace;
         public static MelonPreferences_Entry<int> t64a_chance;
         public override void OnInitializeMelon()
@@ -94,6 +98,8 @@ namespace ATAC
             bmp1_chance = cfg.CreateEntry("BMP-1 replacement chance", 100);
             bmp1p_replace = cfg.CreateEntry("BMP-1P replacement", "BMP1P");
             bmp1p_chance = cfg.CreateEntry("BMP-1P replacement chance", 100);
+            bmp2_replace = cfg.CreateEntry("BMP-2 replacement", "BMP2");
+            bmp2_chance = cfg.CreateEntry("BMP-2 replacement chance", 100);
             t55a_replace = cfg.CreateEntry("T-55A replacement", "T72MGILLS");
             t55a_chance = cfg.CreateEntry("T-55A replacement chance", 100);
             t72m_replace = cfg.CreateEntry("T-72M replacement", "T72A");
@@ -110,10 +116,12 @@ namespace ATAC
             brdm2_sa_chance = cfg.CreateEntry("SA BRDM2 replacement chance", 100);
             btr60pb_sa_replace = cfg.CreateEntry("SA BTR-60PB replacement", "BTR60PB_SA");
             btr60pb_sa_chance = cfg.CreateEntry("SA BTR-60PB replacement chance", 100);
-            bmp1_sa_replace = cfg.CreateEntry("SA BMP-1 replacement", "BMP2_SA");
+            bmp1_sa_replace = cfg.CreateEntry("SA BMP-1 replacement", "BMP1P_SA");
             bmp1_sa_chance = cfg.CreateEntry("SA BMP-1 replacement chance", 100);
-            bmp1p_sa_replace = cfg.CreateEntry("SA BMP-1P replacement", "BMP2_SA");
+            bmp1p_sa_replace = cfg.CreateEntry("SA BMP-1P replacement", "BMP1P_SA");
             bmp1p_sa_chance = cfg.CreateEntry("SA BMP-1P replacement chance", 100);
+            bmp2_sa_replace = cfg.CreateEntry("SA BMP-2 replacement", "BMP2_SA");
+            bmp2_sa_chance = cfg.CreateEntry("SA BMP-2 replacement chance", 100);
             t64a_replace = cfg.CreateEntry("SA T-64A replacement", "T64A");
             t64a_chance = cfg.CreateEntry("SA T-64A replacement chance", 100);
         }
@@ -123,138 +131,119 @@ namespace ATAC
         {
             private static void Postfix(GHPC.Mission.DynamicMissionComposer __instance, ref string __result)
             {
-                ////NATO
-                bool is_M923 = __result == "M923";
-                if (is_M923 && UnityEngine.Random.Range(1, 100) <= m923_chance.Value)
-                { 
-                    __result = m923_replace.Value;
-                }
-                
-                bool is_M113 = __result == "M113";
-                if (is_M113 && UnityEngine.Random.Range(1, 100) <= m113_chance.Value)
+                string replace = null;
+                int chance = 0;
+                bool failed = false;
+                switch (__result)
                 {
-                    __result = m113_replace.Value;
+                    // NATO
+                    case "M923":
+                        replace = m923_replace.Value;
+                        chance = m923_chance.Value;
+                        break;
+                    case "M113":
+                        replace = m113_replace.Value;
+                        chance = m113_chance.Value;
+                        break;
+                    case "M2BRADLEY":
+                        replace = m2_replace.Value;
+                        chance = m2_chance.Value;
+                        break;
+                    case "M60A1RISEP":
+                        replace = m60a1_replace.Value;
+                        chance = m60a1_chance.Value;
+                        break;
+                    case "M60A3TTS":
+                        replace = m60a3_replace.Value;
+                        chance = m60a3_chance.Value;
+                        break;
+                    case "M1":
+                        replace = m1_replace.Value;
+                        chance = m1_chance.Value;
+                        break;
+                    case "M1IP":
+                        replace = m1ip_replace.Value;
+                        chance = m1ip_chance.Value;
+                        break;
+                    // NVA
+                    case "URAL375D":
+                        replace = ural375d_replace.Value;
+                        chance = ural375d_chance.Value;
+                        break;
+                    case "BRDM2":
+                        replace = brdm2_replace.Value;
+                        chance = brdm2_chance.Value;
+                        break;
+                    case "BTR60PB":
+                        replace = btr60pb_replace.Value;
+                        chance = btr60pb_chance.Value;
+                        break;
+                    case "BMP1":
+                        replace = bmp1_replace.Value;
+                        chance = bmp1_chance.Value;
+                        break;
+                    case "BMP1P":
+                        replace = bmp1p_replace.Value;
+                        chance = bmp1p_chance.Value;
+                        break;
+                    case "BMP2":
+                        replace = bmp2_replace.Value;
+                        chance = bmp2_chance.Value;
+                        break;
+                    case "T55A":
+                        replace = t55a_replace.Value;
+                        chance = t55a_chance.Value;
+                        break;
+                    case "T72M":
+                        replace = t72m_replace.Value;
+                        chance = t72mgills_chance.Value;
+                        break;
+                    case "T72MGILLS":
+                        replace = t72mgills_replace.Value;
+                        chance = t72mgills_chance.Value;
+                        break;
+                    case "T72A":
+                        replace = t72m1_replace.Value;
+                        chance = t72m1_chance.Value;
+                        break;
+                    // SA
+                    case "URAL375D_SA":
+                        replace = ural375d_sa_replace.Value;
+                        chance = ural375d_sa_chance.Value;
+                        break;
+                    case "BRDM2_SA":
+                        replace = brdm2_sa_replace.Value;
+                        chance = brdm2_sa_chance.Value;
+                        break;
+                    case "BTR60PB_SA":
+                        replace = btr60pb_sa_replace.Value;
+                        chance = btr60pb_sa_chance.Value;
+                        break;
+                    case "BMP1_SA":
+                        replace = bmp1_sa_replace.Value;
+                        chance = bmp1_sa_chance.Value;
+                        break;
+                    case "BMP1P_SA":
+                        replace = bmp1p_sa_replace.Value;
+                        chance = bmp1p_sa_chance.Value;
+                        break;
+                    case "BMP2_SA":
+                        replace = bmp2_sa_replace.Value;
+                        chance = bmp2_sa_chance.Value;
+                        break;
+                    case "T64A":
+                        replace = t64a_replace.Value;
+                        chance = t64a_chance.Value;
+                        break;
+                    default:
+                        MelonLoader.MelonLogger.Warning("Found a vehicle without a case! Its ID is: " + __result + ". Ignoring.");
+                        failed = true;
+                        break;
                 }
-
-                bool is_M2 = __result == "M2BRADLEY";
-                if (is_M2 && UnityEngine.Random.Range(1, 100) <= m2_chance.Value)
+                if (UnityEngine.Random.Range(1, 100) <= chance && !failed)
                 {
-                    __result = m2_replace.Value;
-                }
-
-                bool is_M60A1 = __result == "M60A1RISEP";
-                if (is_M60A1 && UnityEngine.Random.Range(1, 100) <= m60a1_chance.Value)
-                {
-                    __result = m60a1_replace.Value;
-                }
-
-                bool is_M60A3 = __result == "M60A3TTS";
-                if (is_M60A3 && UnityEngine.Random.Range(1, 100) <= m60a3_chance.Value)
-                {
-                    __result = m60a3_replace.Value;
-                }
-
-                bool is_M1 = __result == "M1";
-                if (is_M1 && UnityEngine.Random.Range(1, 100) <= m1_chance.Value)
-                {
-                    __result = m1_replace.Value;
-                }
-
-                bool is_M1IP = __result == "M1IP";
-                if (is_M1IP && UnityEngine.Random.Range(1, 100) <= m1ip_chance.Value)
-                {
-                    __result = m1ip_replace.Value;
-                }
-
-                ////NVA
-                bool is_URAL375D = __result == "URAL375D";
-                if (is_URAL375D && UnityEngine.Random.Range(1, 100) <= ural375d_chance.Value)
-                {
-                    __result = ural375d_replace.Value;
-                }
-                bool is_BRDM2 = __result == "BRDM2";
-                if (is_BRDM2 && UnityEngine.Random.Range(1, 100) <= brdm2_chance.Value)
-                {
-                    __result = brdm2_replace.Value;
-                }
-
-                bool is_BTR60 = __result == "BTR60PB";
-                if (is_BTR60 && UnityEngine.Random.Range(1, 100) <= btr60pb_chance.Value)
-                {
-                    __result = btr60pb_replace.Value;
-                }
-
-                bool is_BMP1 = __result == "BMP1";
-                if (is_BMP1 && UnityEngine.Random.Range(1, 100) <= bmp1_chance.Value)
-                {
-                    __result = bmp1_replace.Value;
-                }
-
-                bool is_BMP1P = __result == "BMP1P";
-                if (is_BMP1P && UnityEngine.Random.Range(1, 100) <= bmp1p_chance.Value)
-                {
-                    __result = bmp1p_replace.Value;
-                }
-
-                bool is_T55A = __result == "T55A";
-                if (is_T55A && UnityEngine.Random.Range(1, 100) <= t55a_chance.Value)
-                {
-                    __result = t55a_replace.Value;
-                }
-
-                bool is_T72M = __result == "T72M";
-                if (is_T72M && UnityEngine.Random.Range(1, 100) <= t72m_chance.Value)
-                {
-                    __result = t72m_replace.Value;
-                }
-
-                bool is_T72MGILLS = __result == "T72MGILLS";
-                if (is_T72MGILLS && UnityEngine.Random.Range(1, 100) <= t72mgills_chance.Value)
-                {
-                    __result = t72mgills_replace.Value;
-                }
-
-                bool is_T72M1 = __result == "T72A";
-                if (is_T72M1 && UnityEngine.Random.Range(1, 100) <= t72m1_chance.Value)
-                {
-                    __result = t72m1_replace.Value;
-                }
-
-
-                ////SA
-                bool is_URAL375D_SA = __result == "URAL375D_SA";
-                if (is_URAL375D_SA && UnityEngine.Random.Range(1, 100) <= ural375d_sa_chance.Value)
-                {
-                    __result = ural375d_sa_replace.Value;
-                }
-                bool is_BRDM2_SA = __result == "BRDM2_SA";
-                if (is_BRDM2_SA && UnityEngine.Random.Range(1, 100) <= brdm2_sa_chance.Value)
-                {
-                    __result = brdm2_sa_replace.Value;
-                }
-
-                bool is_BTR60_SA = __result == "BTR60PB_SA";
-                if (is_BTR60_SA && UnityEngine.Random.Range(1, 100) <= btr60pb_sa_chance.Value)
-                {
-                    __result = btr60pb_sa_replace.Value;
-                }
-
-                bool is_BMP1_SA = __result == "BMP1_SA";
-                if (is_BMP1_SA && UnityEngine.Random.Range(1, 100) <= bmp1_sa_chance.Value)
-                {
-                    __result = bmp1_sa_replace.Value;
-                }
-
-                bool is_BMP1P_SA = __result == "BMP1P_SA";
-                if (is_BMP1P_SA && UnityEngine.Random.Range(1, 100) <= bmp1p_sa_chance.Value)
-                {
-                    __result = bmp1p_sa_replace.Value;
-                }
-
-                bool is_T64A = __result == "T64A";
-                if (is_T64A && UnityEngine.Random.Range(1, 100) <= t64a_chance.Value)
-                {
-                    __result = t64a_replace.Value;
+                    MelonLoader.MelonLogger.Msg("Transformed a " + __result + " into a " + replace + "!");
+                    __result = replace;
                 }
             }
         }
